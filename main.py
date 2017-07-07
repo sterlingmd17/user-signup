3ab7eaed16b3a9cb7c25a22b273dfd39cfc9d99c
Sterling <sterlingmd17@gmail.com>
2017-07-06 19:29:45 -0700

    cleaned code up more and added comments

--- main.py
+++ main.py
@@ -12,33 +12,38 @@ def username():
     password = request.form["password"]
     verify = request.form["verify"]
     email= request.form["email"]
+
     error1=''
-    if len(user) <4 or user.isalpha()==False:
+    if len(user) <4 or user.isalpha()==False: #verify username
         error1 = 'Please enter a valid username' 
         errors.append(error1)
+
     error2=''
-    if len(password) < 4:
+    if len(password) < 4: #verify password
         error2= "Please enter a valid password of atleast 3 characters."
         errors.append(error2)
+    
     error3=''
-    if verify != password:
+    if verify != password: # verify verification password.
         error3 = "Password does not match"
         errors.append(error3)
+    
     error4=''
-    if "@." not in email and " " in email:
+    if "@." not in email and " " in email: #veryify email.
         error4= "Please enter a valid email"
         errors.append(error4)
     elif len(email)>1 and len(email)<4:
         error4= "Please enter a valid email"
         errors.append(error4)
-    if len(errors) > 0:
+    
+    if len(errors) > 0: # render template with errors
         return render_template("/login.html",error1=error1,error2=error2,error3=error3,
-     error4=error4,user=user, email=email)
-    return render_template("welcome.html", user=user)
+        error4=error4,user=user, email=email)
+
+    return render_template("welcome.html", user=user) # render welcome page if no errors.
 
 @app.route("/", methods = ['GET'])    
 def index():
-    error_encoded = request.args.get("error")
-    return render_template("login.html", error = error_encoded)
+    return render_template("login.html")
 
 app.run()
